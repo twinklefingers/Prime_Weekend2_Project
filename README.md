ab06951dcde4aa94d81a243a4be997f5d6be57a4
twinklefingers <B1-66-ER@Rays-MacBook-Pro.local>
2016-08-08 09:48:40 -0500

    not finished yet

--- README.md
+++ README.md
@@ -5,3 +5,33 @@ This Challenge will require you to setup and run the application in this reposit
 [Refer to the jQuery AJAX lecture notes for installing and running the server]  (https://github.com/kdszafranski/prime-ajax-intro)
 
 These instructions will be the same for all of our node applications.
+
+.
+.
+.
+.
+On the DOM
+
+On the DOM should be:
+
+One person's information
+A series of 22 (or the number of people in the data array) index points with the first person's index highlighted or called out in style differently than the others.
+A 'Next' button and a 'Previous' button
+Clicking on the Next button should navigate to the next person, clicking on the Previous button should navigate to the previous person. The highlighted index point should update also as you click through to other people.
+Person Display
+
+When a person is displayed, show their name, their Github link, and their piece of shoutout feedback. Only one person should be showcased at any given time.
+
+You will need to combine everything you learned this week to accomplish this task, and each of the challenges you have completed this week play a part in this task.
+
+Working Example
+
+Here is a similar example from Zeta so you can see the functionality. It's really ugly, however. The code is also minified (no cheating!):
+
+https://polar-ravine-37299.herokuapp.com/
+
+HARD MODE
+Include a fade out and fade in animation in-between transitioning people.
+
+PRO MODE
+Include a timer that moves to the next person if the user is not clicking on next or prev. If the user clicks on next or prev, the timer should be reset. The timer should transition between people every 10 seconds.
--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,9 +1,74 @@
-$(document).ready(function(){
-    $.ajax({
-      type: "GET",
-      url: "/data",
-      success: function(data){
-        console.log(data);
-      }
-    });
+$(document).ready(function() {
+
+    var studentNum = 0;
+    loadData();
+
+    function loadData() {
+        var dataToLoad = $.ajax({
+            type: "GET",
+            url: "/data",
+            success: function(data) {
+                for (var i = 0; i < data.omicron.length; i++) {
+                    $('.container').append('<li>' + data.omicron[i].name + '</li>',
+                        '<h3>' + 'Git UserName: ' + data.omicron[i].git_username + '</h3>',
+                        '<h4>' + 'Message: ' + data.omicron[i].shoutout + '</h4>');
+                    // dataToLoad = data.omicron;
+                    console.log(data.omicron[i].name);
+
+                }
+                nextStudent();
+                prevStudent();
+            }
+        });
+    }
+
+    // for loop to run through all 17 entries
+
+    //functions we might need
+
+    function loadStudentData() {}
+
+    function nextStudent() {
+        $('.buttons').append('<button class="next">Click for Next</button>');
+    }
+
+    function prevStudent() {
+        $('.buttons').append('<button class="prev">Click for Prev</button>');
+        $('.prev').on('click', function() {
+
+        });
+    }
+
+    function showStudentInfo() {
+        //name
+        //git_username
+        //message
+    }
 });
+
+
+// success: function(response) {
+//   console.log(response.people);
+//   for(var i = 0; i < response.people.length; i++) {
+//     $('#ajax-data').append('<div class="person"></div>');
+//     var $el = $('#ajax-data').children().last();
+//     $el.append('<h2>' + response.people[i].name + '</h2>');
+//     $el.append('<img src="' + response.people[i].imageURL + '" />');
+//   }
+// },
+// error: function() {
+//   console.log('Error with request');
+// }
+
+// var i = 1;
+//
+// function newSpy() {
+//     $("<div />", {
+//         "class": "spy",
+//         id: "newSpy" + i
+//     }).appendTo(".newContainer").text("New Spy" + i);
+//     $(".spy").last().append("<button class='deleter'>Delete Spy</button>");
+//     $(".spy").last().append("<button class='changer'>Change</button>");
+//     $(".spy").last().append("<p style='display: inline'>Total Spies Hired:" + i + "</p>");
+//     i++;
+// }
