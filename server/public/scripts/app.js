06fe43387e6ebe9008a3b14611138626b259b09b
twinklefingers <rachelweila@gmail.com>
2016-12-03 22:51:09 -0600

    added console timer

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -12,7 +12,6 @@ $(document).ready(function() {
 //*****************************
 var i = -1;
 var myTimer = setInterval(loadData, 5000);
-// var data = {};
 
 //*****************************
 //         Functions
@@ -31,13 +30,38 @@ function loadData() {
     });
 }
 
+function carousalRules(data) {
+
+    // if the carousal is between 0 and the length of messages, show the person's info and increase counter variable i
+    if (i >= -1 && i <= data.omicron.length) {
+        i++;
+        // console.log("moving forward", i);
+    }
+
+    // pressing prev, if i less than 0, it goes to the end
+    // before spinning back around in a positive direction
+    if (i < -1) {
+        i = data.omicron.length - 1; // i=16
+        // console.log("going backwards", i);
+    }
+
+    // if i is greater than the length of data.omicron -1 (16),
+    // i resets at 0
+    if (i > data.omicron.length - 1) {
+        i = 0;
+        // console.log("starting over", i);
+    }
+
+    // console.log("carousal position: ", i);
+}
+
 function showStudentData(data) {
     $('.textView').empty();
     $('.textView').append('<p class="name">' + data.omicron[i].name + '</p>',
         '<p class="gitHub">' + 'Git UserName: ' + data.omicron[i].git_username + '</p>',
         '<p class="message">' + 'Message: ' + data.omicron[i].shoutout + '</p>');
     // carousalRules(data);
-    console.log("showData", data.omicron[i].name, i);
+    // console.log("showData", data.omicron[i].name, i);
 }
 
 function next() {
@@ -45,7 +69,7 @@ function next() {
     loadData();
     clearInterval(myTimer);
     myTimer = setInterval(loadData, 5000);
-    console.log("You pressed Next!", i);
+    // console.log("You pressed Next!", i);
 }
 
 function prev() {
@@ -53,32 +77,23 @@ function prev() {
     loadData();
     clearInterval(myTimer);
     myTimer = setInterval(loadData, 5000);
-    console.log("You pressed Prev!", i);
+    // console.log("You pressed Prev!", i);
 }
 
-function carousalRules(data) {
-  // if the carousal is between 0 and the length of messages, show the person's info and increase counter variable i
-  if (i >= -1 && i <= data.omicron.length) {
-      // showStudentData(data);
-      i++;
-      console.log("moving forward", i);
-  }
 
-  // going backwards, if i less than 0, it goes to the end
-  // before spinning back around in a positive direction
-  if (i < -1) {
-      i = data.omicron.length - 1; // i=15
-      console.log("going backwards", i);
-  }
+//********************************************************
+//         Weird Little Timer I Added to Console
+//********************************************************
+var counter = 6;
+var consoleTimer = setInterval(function() {
+    countdown();
+}, 1000);
 
-    // if i is greater than the length of data.omicron -1 (16),
-    // i resets at 0
-    if (i > data.omicron.length - 1) {
-        i = 0;
-        console.log("starting over", i);
+function countdown() {
+    counter--;
+    console.log(counter);
+    if (counter === 1) {
+        counter = 6;
     }
-
-
-
-    console.log("carousal position: ", i);
 }
+// *******************************************************
