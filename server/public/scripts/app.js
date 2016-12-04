7eac336998453d908471009c0d8dd73408797560
twinklefingers <rachelweila@gmail.com>
2016-12-03 21:56:30 -0600

    working on prev button

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -11,59 +11,75 @@ $(document).ready(function() {
 //         Variables
 //*****************************
 var i = 0;
-var data;
+var myTimer = setInterval(loadData, 5000);
+// var data = {};
 
 //*****************************
 //         Functions
 //*****************************
 
-// this loadData function is very important
+// this loadData function is very important, it's like the glue between unique functions,
+// and the data in the json file...
 function loadData() {
     var data = $.ajax({
         type: "GET",
         url: "/data",
         success: function(data) {
-            loadStudentData(data);
-            showPerson(data);
+            showStudentData(data);
+            // carousalRules(data);
         }
     });
 }
 
-function loadStudentData(data) {
-    loadOne();
-    setInterval(loadOne, 5000);
-
-    function loadOne() {
-        console.log(i);
-        if (i >= data.omicron.length) {
-            i = 0;
-        }
-        if (i >= 0 && i <= data.omicron.length) {
-            showPerson(data);
-            console.log(data.omicron[i].name, i);
-            i++;
-        }
-    }
-
-    function showPerson(data) {
-        $('.textView').empty();
-        $('.textView').append('<p class="name">' + data.omicron[i].name + '</p>',
-            '<p class="gitHub">' + 'Git UserName: ' + data.omicron[i].git_username + '</p>',
-            '<p class="message">' + 'Message: ' + data.omicron[i].shoutout + '</p>');
-    }
+function showStudentData(data) {
+    // i++;
+    var studentName = data.omicron[i].name;
+    console.log(studentName, i);
+    $('.textView').empty();
+    $('.textView').append('<p class="name">' + studentName + '</p>',
+        '<p class="gitHub">' + 'Git UserName: ' + data.omicron[i].git_username + '</p>',
+        '<p class="message">' + 'Message: ' + data.omicron[i].shoutout + '</p>');
+    carousalRules(data);
+    console.log("showData", i);
 }
 
-
 function next() {
-    clearInterval();
-    i++;
-    console.log("You pressed Next!");
-    console.log(i);
+    // i++;
+    loadData();
+    clearInterval(myTimer);
+    myTimer = setInterval(loadData, 5000);
+    console.log("You pressed Next!", i);
 }
 
 function prev() {
-    showPerson();
-    i--;
-    console.log("You pressed Prev!");
-    console.log(i);
+    i = i - 2;
+    loadData();
+    clearInterval(myTimer);
+    myTimer = setInterval(loadData, 5000);
+    console.log("You pressed Prev!", i);
+}
+
+function carousalRules(data) {
+    // if the carousal is between 0 and the length of messages, show the person's info and increase counter variable i
+    if (i >= 0 && i <= data.omicron.length) {
+        i++;
+        console.log("moving forward", i);
+        // showStudentData(data);
+    }
+
+    // if i is greater than the length of data.omicron -1 (16),
+    // i resets at 0
+    if (i > data.omicron.length - 1) {
+        i = 0;
+        console.log("starting over", i);
+    }
+
+    // going backwards, if i less than 0, it goes to the end
+    // before spinning back around in a positive direction
+    if (i < 0) {
+        i = data.omicron.length - 1;
+        console.log("going backwards", i);
+    }
+    console.log("carousal position: ", i);
+
 }
