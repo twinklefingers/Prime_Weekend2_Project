0522d71bce6bad406c6c8414f1d954526cc5ca27
twinklefingers <rachelweila@gmail.com>
2016-12-03 20:13:12 -0600

    working on prev/next

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
 
     loadData();
+
+    //event listeners
+    $('.nextButton').on("click", next);
+    $('.prevButton').on("click", prev);
 }); // end doc ready
 
 //*****************************
 //         Variables
 //*****************************
-var studentNum = 0;
-
+var i = 0;
+var data;
 
 //*****************************
 //         Functions
@@ -15,58 +19,51 @@ var studentNum = 0;
 
 // this loadData function is very important
 function loadData() {
-    var dataToLoad = $.ajax({
+    var data = $.ajax({
         type: "GET",
         url: "/data",
         success: function(data) {
             loadStudentData(data);
+            showPerson(data);
         }
     });
 }
 
 function loadStudentData(data) {
-    var i = 0;
     loadOne();
     setInterval(loadOne, 5000);
 
     function loadOne() {
-        i++;
+        console.log(i);
+        if (i >= data.omicron.length) {
+            i = 0;
+        }
+        if (i >= 0 && i <= data.omicron.length) {
+            showPerson(data);
+            console.log(data.omicron[i].name, i);
+            i++;
+        }
+    }
+
+    function showPerson(data) {
         $('.textView').empty();
         $('.textView').append('<p class="name">' + data.omicron[i].name + '</p>',
             '<p class="gitHub">' + 'Git UserName: ' + data.omicron[i].git_username + '</p>',
             '<p class="message">' + 'Message: ' + data.omicron[i].shoutout + '</p>');
-        console.log(data.omicron[i].name);
     }
 }
 
 
-function nextStudent() {
-    $('.buttons').append('<button class="next">Click for Next</button>');
+function next() {
+    clearInterval();
+    i++;
+    console.log("You pressed Next!");
+    console.log(i);
 }
 
-function prevStudent() {
-    $('.buttons').append('<button class="prev">Click for Prev</button>');
-    $('.prev').on('click', function() {
-
-    });
+function prev() {
+    showPerson();
+    i--;
+    console.log("You pressed Prev!");
+    console.log(i);
 }
-
-function showStudentInfo() {
-    //name
-    //git_username
-    //message
-}
-
-
-// success: function(response) {
-//   console.log(response.people);
-//   for(var i = 0; i < response.people.length; i++) {
-//     $('#ajax-data').append('<div class="person"></div>');
-//     var $el = $('#ajax-data').children().last();
-//     $el.append('<h2>' + response.people[i].name + '</h2>');
-//     $el.append('<img src="' + response.people[i].imageURL + '" />');
-//   }
-// },
-// error: function() {
-//   console.log('Error with request');
-// }
